58fdf54a03bfb2a6e79e15ebb24ee858edaf8ad8
Subham Mohanty <mohantysubham853@gmail.com>
2024-08-20 12:55:16 +0530

    first commit

---
+++ src/main/java/com/subham/movie/service/MovieService.java
@@ -0,0 +1,23 @@
+package com.subham.movie.service;
+
+import com.subham.movie.model.Movie;
+import com.subham.movie.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+    public List<Movie> findAllMovies() {
+        return repository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return repository.findMovieByImdbId(imdbId);
+    }
+}
---
+++ src/main/java/com/subham/movie/service/ReviewService.java
@@ -0,0 +1,33 @@
+package com.subham.movie.service;
+
+
+import com.subham.movie.model.Movie;
+import com.subham.movie.model.Review;
+import com.subham.movie.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = repository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review.getId()))
+                .first();
+        return review;
+    }
+}
